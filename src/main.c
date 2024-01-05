5930bcb06cda81f024373fdaf669bbbc9d523dd4
j_delsol_catie <j.delsol@catie.fr>
2024-01-05 17:45:22 +0100

    WIP: add sample fetch function

--- src/main.c
+++ src/main.c
@@ -1,13 +1,17 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/sensor.h>
 
+#include "hpma115.h"
+
 #define SLEEP_TIME_MS   1000
 
 void main(void)
 {
     int ret;
-    struct sensor_value val;
     const struct device *sensor;
+    struct sensor_value val;
+
+    k_msleep(SLEEP_TIME_MS);
     sensor = DEVICE_DT_GET(DT_NODELABEL(hpma115));
 
     if (!device_is_ready(sensor)) {
@@ -15,18 +19,22 @@ void main(void)
         return 0;
     }
 
+    k_msleep(SLEEP_TIME_MS);
+
+    ret = sensor_sample_fetch(sensor);
+
     while (1) {
-        //printk("Alive!\n");
-        ret = sensor_channel_get(sensor, SENSOR_CHAN_PM_1_0, &val);
-		if (ret < 0) {
-			printk("Could not get sample (%d)", ret);
-			return 0;
-		}
-
-		printk("Sensor value: %d\n", val.val1);
-
+       
+        // ret = sensor_channel_get(sensor, SENSOR_CHAN_PM_1_0, &val);
+        // printk("PM1_0: %d\n", val.val1);
+        // ret = sensor_channel_get(sensor, SENSOR_CHAN_PM_2_5, &val);
+        // printk("PM2_5: %d\n", val.val1);
+        // ret = sensor_channel_get(sensor, SENSOR_CHAN_PM_10, &val);
+        // printk("PM10: %d\n", val.val1);
+		// if (ret < 0) {
+		// 	printk("Could not get sample (%d)", ret);
+		// 	return 0;
+		// }        
         k_msleep(SLEEP_TIME_MS);
     }
-    
-
 }
